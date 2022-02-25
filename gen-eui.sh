fb049130060692c87abd0aedb41a09cf25a2a1be
RightToPrivacy <58191050+RightToPrivacy@users.noreply.github.com>
2022-02-25 02:29:25 -0500

    Update gen-eui.sh

--- gen-eui.sh
+++ gen-eui.sh
@@ -2,10 +2,16 @@
 #
 # simple command to generate valid/random new EUI
 #
-# prints out configuration file for you to edit
+# prints out configuration file for optional editing + restart instructions
 #
 # righttoprivacy@tutanota.com
 #
+
+# TEXT COLORS
+export BLUE='\033[1;94m'
+export GREEN='\033[1;92m'
+export RED='\033[1;91m'
+export ENDCOLOR='\033[1;00m'
 
 # DEFAULT OUI
 oui='02BA07'
@@ -15,12 +21,12 @@ end=$(xxd -u -l 3 -p /dev/urandom)
 # CONFIGURATION FILE FOR EUI
 localconf='/opt/ttn-gateway/packet_forwarder/lora_pkt_fwd/local_conf.json'
 
-echo -e "GENERATING YOU A NEW EUI...\n" && sleep 1
+echo -e "\n${BLUE}GENERATING YOU A NEW EUI...${ENDCOLOR}\n" && sleep 1
 
 newEUI="$oui$mid$end"
 sleep .5
 
-echo -e "NEW EUI IS $newEUI.\n"
+echo -e "${BLUE}EUI GENERATED:${ENDCOLOR} ${RED}$newEUI${ENDCOLOR}\n"
 
-echo -e "You can replace current in $localconf\n" && sleep .5
-echo -e "Then restart gateway."
+echo -e "${BLUE}You can replace current in:${ENDCOLOR} ${GREEN}$localconf${ENDCOLOR}\n" && sleep .5
+echo -e "${BLUE}Then restart gateway by issuing:${ENDCOLOR} ${RED}sudo systemctl restart ttn-gateway${ENDCOLOR}\n"
